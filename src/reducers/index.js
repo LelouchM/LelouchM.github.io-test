5cc9a39400b829134b26a0461f9cee4ce99ac707
Lelouch <gledich@tut.by>
2017-05-03 14:35:36 +0300

    Fix

--- src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-import { routerReducer } from 'react-router-redux';
-
-import reducer from './Reducer';
-
-export default combineReducers({
-  routing: routerReducer,
-  reducer,
-});
